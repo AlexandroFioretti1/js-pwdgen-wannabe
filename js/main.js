f03539f0a037ad088ab3256c76448aba8433ab6f
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-03-07 14:19:00 +0100

    add name\surname\color.(js)

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-alert("funziona")
+//alert("funziona") //
 
 /*
 Decrizione passaggi: 
-
 1)Chiedi all'utente il suo nome,
 
 2)poi chiedi il suo cognome,
@@ -10,5 +9,20 @@ Decrizione passaggi:
 3)poi chiedi il suo colore preferito
 
 4)Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23 
-
 */
+
+/******************************************************************
+******************************************************************/
+
+/* 1)Chiedi all'utente il suo nome,*/
+let name = prompt("what\'s your Name?");
+console.log(name);
+
+/* 2)poi chiedi il suo cognome,*/
+let surname = prompt("what\'s your Surname?")
+console.log(surname);
+
+/* 3)poi chiedi il suo colore preferito*/
+
+let color = prompt("what is your favorite color")
+console.log(color);
