fbecff98bdb57dbb756199f3e16665f92c627bc7
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-03-07 14:08:53 +0100

    aggiunta traccia

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 alert("funziona")
+
+/*
+Decrizione passaggi: 
+
+1)Chiedi all'utente il suo nome,
+
+2)poi chiedi il suo cognome,
+
+3)poi chiedi il suo colore preferito
+
+4)Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23 
+
+*/
