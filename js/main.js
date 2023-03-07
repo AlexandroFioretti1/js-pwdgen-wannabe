dded75accdb35c1ffa4d963e33cc090811c252e8
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-03-07 14:27:37 +0100

    add results

--- js/main.js
+++ js/main.js
@@ -12,17 +12,24 @@ Decrizione passaggi:
 */
 
 /******************************************************************
-******************************************************************/
+ ******************************************************************/
 
 /* 1)Chiedi all'utente il suo nome,*/
-let name = prompt("what\'s your Name?");
+let name = prompt("what's your Name?");
 console.log(name);
 
 /* 2)poi chiedi il suo cognome,*/
-let surname = prompt("what\'s your Surname?")
+let surname = prompt("what's your Surname?");
 console.log(surname);
 
 /* 3)poi chiedi il suo colore preferito*/
+let preferColor = prompt("what is your favorite color");
+console.log(preferColor);
 
-let color = prompt("what is your favorite color")
-console.log(color);
+/* 4)Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23 */
+let result = name + surname + preferColor + "23"
+document.write("result" + result)
+console.log(result);
+
+/******************************************************************
+ ******************************************************************/
